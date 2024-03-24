47e3a8ed931335aa0cc3bcd8beec1fe5856cf674
Amy Tian <z5425443@ad.unsw.edu.au>
2024-03-24 12:40:53 +1100

    simplify logic in MoveInvincible.move()

--- app/src/main/java/dungeonmania/entities/enemies/enemyMoveStrategy/MoveInvincible.java
+++ app/src/main/java/dungeonmania/entities/enemies/enemyMoveStrategy/MoveInvincible.java
@@ -7,7 +7,6 @@ import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class MoveInvincible implements MoveStrategy {
-
     @Override
     public void move(Game game, Enemy enemy) {
         GameMap map = game.getMap();
@@ -18,25 +17,20 @@ public class MoveInvincible implements MoveStrategy {
         Position moveY = (plrDiff.getY() >= 0) ? Position.translateBy(enemy.getPosition(), Direction.UP)
                 : Position.translateBy(enemy.getPosition(), Direction.DOWN);
         Position offset = enemy.getPosition();
+
         if (plrDiff.getY() == 0 && map.canMoveTo(enemy, moveX))
             offset = moveX;
         else if (plrDiff.getX() == 0 && map.canMoveTo(enemy, moveY))
             offset = moveY;
-        else if (Math.abs(plrDiff.getX()) >= Math.abs(plrDiff.getY())) {
+        else {
             if (map.canMoveTo(enemy, moveX))
                 offset = moveX;
             else if (map.canMoveTo(enemy, moveY))
                 offset = moveY;
             else
                 offset = enemy.getPosition();
-        } else {
-            if (map.canMoveTo(enemy, moveY))
-                offset = moveY;
-            else if (map.canMoveTo(enemy, moveX))
-                offset = moveX;
-            else
-                offset = enemy.getPosition();
         }
+
         map.moveTo(enemy, offset);
     }
 }
