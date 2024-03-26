b3c70ce73f48fdef525d69abf21dc47fd6252070
Amy Tian <z5425443@ad.unsw.edu.au>
2024-03-26 12:38:38 +1100

    update default behaviour of canMoveOnTo

--- app/src/main/java/dungeonmania/entities/collectables/potions/Potion.java
+++ app/src/main/java/dungeonmania/entities/collectables/potions/Potion.java
@@ -3,10 +3,8 @@ package dungeonmania.entities.collectables.potions;
 import dungeonmania.Game;
 import dungeonmania.battles.BattleStatistics;
 import dungeonmania.entities.BattleItem;
-import dungeonmania.entities.Entity;
 import dungeonmania.entities.collectables.Collectables;
 import dungeonmania.entities.playerState.PlayerState;
-import dungeonmania.map.GameMap;
 import dungeonmania.util.Position;
 
 public abstract class Potion extends Collectables implements BattleItem {
@@ -16,12 +14,6 @@ public abstract class Potion extends Collectables implements BattleItem {
         super(position);
         this.duration = duration;
     }
-
-    @Override
-    public boolean canMoveOnto(GameMap map, Entity entity) {
-        return true;
-    }
-
 
     @Override
     public void use(Game game) {
